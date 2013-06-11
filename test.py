00886a694e57f6cf310a96976519d8731af373c1
Krris <krrisgit@gmail.com>
2013-06-12 00:34:52 +0200

    Uncomment test.py.

--- test.py
+++ test.py
@@ -5,21 +5,21 @@ import os
 temperatures = ['10', '100', '1000', '10000']
 maxDepths = ['5', '10', '20', '30']
 
-# # test cars
-# print "Start testing cars..."
-# params = ["Horsepower", "Cylinders", "Displacement", "Weight", 
-#           "Acceleration", "Model"]
-# 
-# for depth in maxDepths:
-#     for temp in temperatures:
-#         for param in params:
-#             os.system("./annealing.py -i cars.csv -p " + param + 
-#                       " -t " + temp +" -s 1 -d " + depth)
-# 
-# print "End testing cars."
-# 
-# # test cameras
-# print "Start testing cameras..."
+# test cars
+print "Start testing cars..."
+params = ["Horsepower", "Cylinders", "Displacement", "Weight", 
+          "Acceleration", "Model"]
+ 
+for depth in maxDepths:
+    for temp in temperatures:
+        for param in params:
+            os.system("./annealing.py -i cars.csv -p " + param + 
+                      " -t " + temp +" -s 1 -d " + depth)
+ 
+print "End testing cars."
+ 
+# test cameras
+print "Start testing cameras..."
 
 params = ["Release_date", "Max_resolution", "Low_resolution","Effective_pixels",
           "Zoom_wide","Zoom_tele","Normal_focus_range",
